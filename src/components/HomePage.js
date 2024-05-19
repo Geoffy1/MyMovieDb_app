0a8eb5d801673a5eb4ecc2b96d2bd72836c2625f
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2024-05-19 19:57:15 +0300

    Changed logo to Movie Kudonjo

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -109,7 +109,7 @@ function HomePage() {
       >
         {/* Navbar */}
         <div className="navbar">
-          <div className="logo"><h2>Kamovie mbwexe</h2></div>
+          <div className="logo"><h2>Movie Kudonjo</h2></div>
           
           <div className="search-bar">
             <input
